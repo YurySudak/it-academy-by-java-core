5fea513a7a9d0b95619b050bee8a8a7e61fa3975
Yury <45999929+YurySudak@users.noreply.github.com>
2020-10-01 19:59:39 +0300

    fix: task 3 lesson 1 fixed

--- Lesson1/Task3.java
+++ Lesson1/Task3.java
@@ -3,6 +3,6 @@ package Lesson1;
 public class Task3 {
     public static void main(String[] args) {
         System.out.println("Hello Belarus!");
-        System.out.println("I like Java!!");
+        System.out.println("I like Java!");
     }
 }
